a989af85244298b344b4847bb3a28919ff947286
Maksym <praniknumberone@gmail.com>
2026-02-12 02:30:26 +0100

    DoneMerge

--- assets/js/array.js
+++ assets/js/array.js
@@ -1,3 +1,4 @@
+console.log(`--Exercice1--`);
 class User {
   #id;
   #firstName;
@@ -174,4 +175,76 @@ try {
   const badUser = new User(102, "", "Doe", "Street");
 } catch (error) {
   console.error(">>> ПЕРЕХОПЛЕНО ПОМИЛКУ:", error.message);
+}
+console.log(`--Exercice2--`);
+class Animal {
+  #name;
+
+  constructor(name) {
+    if (new.target === Animal) {
+      throw new TypeError(
+        "Неможливо створити екземпляр абстрактного класу Animal. Використовуйте Tiger або Wolf.",
+      );
+    }
+    this.name = name;
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(value) {
+    if (typeof value !== "string") {
+      throw new TypeError(
+        `Помилка: Ім'я тварини має бути рядком, отримано ${typeof value}.`,
+      );
+    }
+    const emptyName = value.trim();
+    if (emptyName.length === 0) {
+      throw new RangeError("Помилка: Ім'я тварини не може бути порожнім.");
+    }
+    this.#name = emptyName;
+  }
+  hunting() {
+    console.log(`${this.name}: Зараз дожену здобич...`);
+  }
+  growl() {
+    console.log(`${this.name}: Гррррр!`);
+  }
+}
+class Tiger extends Animal {
+  hunting() {
+    console.log(` ${this.name} (Тигр): Я крадусь у високій траві... Стрибок!`);
+  }
+
+  growl() {
+    console.log(` ${this.name} (Тигр): РРР-МЯУ! Тигр з’їсть тебе!`);
+  }
+}
+class Wolf extends Animal {
+  hunting() {
+    console.log(` ${this.name} (Вовк): Ми оточуємо здобич зграєю!`);
+  }
+
+  growl() {
+    console.log(` ${this.name} (Вовк): АУУУУУ! (Виття на місяць)`);
+  }
+}
+try {
+  console.log("--- 1. Створення коректних об'єктів ---");
+  const shereKhan = new Tiger("Шерхан");
+  const akela = new Wolf("Акела");
+  shereKhan.hunting();
+  shereKhan.growl();
+  akela.hunting();
+  akela.growl();
+  console.log("\n--- 2. Перевірка валідації (помилка імені) ---");
+  const badWolf = new Wolf("   ");
+} catch (error) {
+  console.error(">>> СПІЙМАНО ПОМИЛКУ:", error.message);
+}
+
+try {
+  console.log("\n--- 3. Перевірка абстрактності ---");
+  const unknown = new Animal("Чупакабра");
+} catch (error) {
+  console.error(">>> СПІЙМАНО ПОМИЛКУ:", error.message);
 }
